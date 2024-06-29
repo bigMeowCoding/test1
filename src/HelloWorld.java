2a071cdd8524ac3ae51a05d9311bc76e902955c0
周义竣 <zhouyijun@bwcj.com>
2024-06-29 23:09:23 +0800

    feat: helloworld

---
+++ src/HelloWorld.java
@@ -0,0 +1,48 @@
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+//        int n1;
+//        n1 = 13;
+//        int n2;
+//        n2 = 17;
+//        int n3;
+//        n3 = n1 + n2;
+//        System.out.println(n3);
+//        int n4 = 38;
+//        int n5 = n4 - n3;
+//        System.out.println(n5);
+
+//        char c1 = '\n';
+//        char c2 = '\t';
+//        char c3 = '\r';
+//        char c4 = '\\';
+//        char c5 = '1';
+//        char c6 = '2';
+//        char c7 = '3';
+//        System.out.println(c1);
+//        System.out.println(c2);
+//        System.out.println(c3);
+//        System.out.println(c4);
+//        System.out.println(c5);
+//        System.out.println(c6);
+//        System.out.println(c7);
+//        String b1 = "天龙八部";
+//        String b2 = "射雕英雄传";
+//        System.out.println(b1 + b2);
+//        char c1 = '男';
+//        char c2 = '女';
+//        double p1 = 10.24;
+//        double p2 = 20.333;
+//        System.out.println(c1 + c2);
+//        System.out.println(p1 + p2);
+
+        String name = "张三";
+        int age = 18;
+        float score = 92;
+        char sexy = '男';
+        String happy = "看书";
+        System.out.print("姓名\t年龄\t成绩\t性别\t爱好\n");
+        System.out.printf("%s\t%d\t%2.0f\t%c\t%s", name, age, score, sexy, happy);
+    }
+}
